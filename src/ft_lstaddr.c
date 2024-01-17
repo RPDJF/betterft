6d4880b152d98311bd534993f799e2b09165cb91
rpdjf <ruipaulo.unify@outlook.fr>
2024-01-17 19:49:56 +0100

    fix ft_lstaddr

--- src/ft_lstaddr.c
+++ src/ft_lstaddr.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rude-jes <ruipaulo.unify@outlook.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 15:17:11 by rude-jes          #+#    #+#             */
+/*   Created: 2024/01/17 19:49:11 by rude-jes          #+#    #+#             */
 /*   Updated: 2024/01/05 03:19:03 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../betterft.h"
 
-t_list	*ft_lstadd(t_list *lst, void *content)
+t_list	*ft_lstaddr(t_list *lst, void *content)
 {
 	t_list	*newlist;
 
